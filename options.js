b22d60f41e35b96d9c549aeb960d930ecd0ae99a
Omar El-Dalil <omar.eldaleel@gmail.com>
2018-04-14 20:14:11 +0200

    add multiple departments

--- options.js
+++ options.js
@@ -1,11 +1,15 @@
 document.querySelector('input[type=submit]').addEventListener('click', function (e) {
     var name = document.querySelector('#agentName').value,
-        whereToFill = document.querySelector('select').value;
+        whereToFill = document.querySelector('select#whereToFill').value,
+        department = document.querySelector('select#department').value;
     chrome.storage.local.set({agentName : name }, function () {
         getAndUpdateSomeValue('agentName');
     });
     chrome.storage.local.set({whereToFill : whereToFill }, function () {
         getAndUpdateSomeValue('whereToFill');
+    });
+    chrome.storage.local.set({department : department }, function () {
+        getAndUpdateSomeValue('department');
     });
     var el = document.createElement('p'),
         txt= document.createTextNode('Saved');
@@ -21,3 +25,4 @@ var getAndUpdateSomeValue = function(value){
 }
 getAndUpdateSomeValue('agentName');
 getAndUpdateSomeValue('whereToFill');
+getAndUpdateSomeValue('department');
